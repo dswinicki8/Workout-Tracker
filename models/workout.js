5a1bd1477aae4f966126cec0fbe348b59f7668cc
Drew Swinicki <dswinicki93@gmail.com>
2020-07-05 21:13:07 -0400

    changed file layout

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutsSchema = new Schema({
+
+  day: {
+    type: Date,
+    default: () => new Date()
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Choose resistance or cardio style workout."
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Input the name of the exercise."
+      },
+      duration: {
+        type: Number,
+        required: "How long did the workout last?"
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
+});
+
+const Workouts = mongoose.model("Workouts", workoutsSchema);
+
+module.exports = Workouts;
+
+
+
+
+
+
---
+++ routes/apiroutes.js
@@ -0,0 +1,39 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+
+  //GET Route//
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.json(err)
+      });
+  });
+
+  //POST Route//
+  app.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.json(err)
+      });
+  });
+//PUT Route//
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.json(err)
+      });
+  });
+};
